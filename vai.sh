e974d3e6f878a6f159c146fe33978cbc1bbcdff4
byosni <byosni@gmail.com>
2017-07-06 15:15:08 -0300

    Add files via upload

---
+++ vai.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#script para caputar dos dados de Stats do Bind e criar o arquivo para o PRTG.
+#criado por Osni Silva.
+
+#faz download do arquivo named.log do meu servidor bind.
+lftp -e "mirror /var/log/named /var/www/html;quit" -p 2222 -u login,senha sftp://ipdoservidordedns-bind
+
+#limpa o arquivo /var/www/html/prtg.txt
+rm -rf /var/www/html/prtg.txt
+
+#pega o nome dos arquivos e gera os canais
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "A" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "NS" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "CNAME" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "SOA" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "PTR" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "HINFO" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "MX" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "TXT" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "AAAA" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
+echo ; grep "Cache DB" /var/www/html/named.log -A 16 | grep "SRV" | awk '{print "[" $1 "]" }' | sed -n '1p' >> /var/www/html/prtg.txt
